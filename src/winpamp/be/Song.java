259647609dddf05f5afc531623e65119535f86ba
cilledyr <47023263+cilledyr@users.noreply.github.com>
2019-12-12 19:28:18 +0100

    Updated commentary

--- src/winpamp/be/Song.java
+++ src/winpamp/be/Song.java
@@ -9,7 +9,7 @@ import winpamp.gui.MainModel;
 
 /**
  *
- * @author filip
+ * @author filip, Cecilia, Reza and Francesco
  */
 public class Song {
     private String Name;
@@ -21,6 +21,8 @@ public class Song {
     private int Id;
     private int Row;
     private MainModel model;
+    
+    //when creating a new Song, define all the characteristics.
     public Song(String name,String artist,String category,String time,String file,int id) {
         this.Name = name;
         this.Artist = artist;
@@ -29,60 +31,82 @@ public class Song {
         this.File = file;
         this.Id = id;
     }
+    
+    //Method to return the name of the song
      public String getName() {
         return Name;
     }
 
+     //Method to set the name of the song.
     public void setName(String name) {
         this.Name = name;
     }
+    
+    //Method to get the id of the song.
     public int getId()
     {
         return Id;
     }
+    
+    //Method to set a row number when in a playlist.
     public void setRow(int row)
     {
         this.Row = row;
     }
+    
+    //Method to return the row of the song within a playlist.
     public int getRow()
     {
         return Row;
     }
     
-    
+    //Method to get the file location.
     public String getFile()
     {
         return File;
     }
     
+    
+    //Method to set the file location
     public void setFile(String file)
     {
         this.File = file;
     }
 
+    //Method to set the artist.
     public void setArtist(String artist) {
         this.Artist = artist;
     }
 
+    //Method to set the Category
     public void setCategory(String category) {
       this.Category = category;
     }
 
+    //Method to set the duration time of the song
     public void setTime(String time) {
       this.Time = time;
     }
+    
+    //Method to get the duration time of the song
     public String getTime()
     {
         return Time;
     }
+    
+    //Method to get the category of the song
     public String getCategory()
     {
         return Category;
     }
+    
+//Method to get the artist of the song
     public String getArtist()
     {
         return Artist;
     }
+    
+    
     @Override
     public String toString() {
        model = MainModel.GetInstance();
--- src/winpamp/dal/DalController.java
+++ src/winpamp/dal/DalController.java
@@ -32,14 +32,14 @@ import winpamp.gui.MainModel;
 
 /**
  *
- * @author filip
+ * @author filip, Cecilia, Reza and Francesco
  */
 public class DalController {
-     SQLServerDataSource ds;
+     SQLServerDataSource ds; //The database we are using
       private MainModel model;
     public DalController()
     {
-        
+        //Setting up all the info we need to connect to the databse we have set up.
         ds = new SQLServerDataSource();
         ds.setDatabaseName("Winpamp");
         ds.setUser("CSe19B_6");
@@ -48,60 +48,64 @@ public class DalController {
         ds.setServerName("10.176.111.31");
     }
      
-
+    //Method to edit a single song
     public Song EditSong(Song song, String name, String artist, String category, String time, String filelocation) throws SQLServerException, SQLException  {
         
-       System.out.println("EditSong working");
-        
+    
         try(Connection con = ds.getConnection()){
-            String sqlIf = "UPDATE ALLSONGS SET name = ?, artist = ?, category = ?, time = ?, Filelocation = ? WHERE id=?";
+            String sqlIf = "UPDATE ALLSONGS SET name = ?, artist = ?, category = ?, time = ?, Filelocation = ? WHERE id=?"; //SQL statement to alter the table, identifying the post through song ID.
             PreparedStatement pstmt = con.prepareStatement(sqlIf);
+            //Set the ? values on the SQL statement
             pstmt.setString(1, "" + name + "");
             pstmt.setString(2, "" + artist + "");
             pstmt.setString(3, "" + category + "");
             pstmt.setString(4, "" + time + "");
             pstmt.setString(5, "" + filelocation + "");
             pstmt.setInt(6,song.getId());
-            pstmt.execute();
-        return new Song(name,artist,category,time,filelocation,song.getId());
+            pstmt.execute(); //Execute the statement
+        return new Song(name,artist,category,time,filelocation,song.getId());//Return the song so that we can also update our local instance of the business entity song
     }
     }
-    
+    //Method to add a new song
     public Song NewSong(String name, String artist, String category, String time, String filelocation) throws SQLException {
    
         try(Connection con = ds.getConnection()){
-            String sqlIf = "INSERT INTO ALLSONGS (Name, Artist, Category, Time, Filelocation) VALUES (?, ?, ?, ?, ?);";
-            PreparedStatement pstmt = con.prepareStatement(sqlIf,Statement.RETURN_GENERATED_KEYS);
+            String sqlIf = "INSERT INTO ALLSONGS (Name, Artist, Category, Time, Filelocation) VALUES (?, ?, ?, ?, ?);"; //The SQL statement. We don't need an id, as the id is auto incremented.
+            PreparedStatement pstmt = con.prepareStatement(sqlIf,Statement.RETURN_GENERATED_KEYS);//Prepare the statement, and make sure to get the generated key returned.
+            //Add the actual info for the ? in the prepared statement.
             pstmt.setString(1, "" + name + "");
             pstmt.setString(2, "" + artist + "");
             pstmt.setString(3, "" + category + "");
             pstmt.setString(4, "" + time + "");
             pstmt.setString(5, "" + filelocation + "");
-            pstmt.execute();
-            ResultSet rs = pstmt.getGeneratedKeys();
+            pstmt.execute(); //Execute the statment.
+            ResultSet rs = pstmt.getGeneratedKeys(); //Store the return form executing the statement.
             rs.next();
-            int id = rs.getInt(1);
-            return new Song(name,artist,category,time,filelocation,id);
+            int id = rs.getInt(1);//Store the id of the song, which we got returned from the statement, into an int.
+            return new Song(name,artist,category,time,filelocation,id);//Return all the info of the song, so that we can store it in our local instance of the business entity Song.
         } 
     }
     
+    //Method to fetch all songs from the Database.
      public List<Song> getAllSongs() 
     { 
-        List<Song> songs = new ArrayList();
+        List<Song> songs = new ArrayList();//create a list to store all our songs
         try (Connection con = ds.getConnection()){
-            String sqlStatement = "SELECT * FROM ALLSONGS";
+            String sqlStatement = "SELECT * FROM ALLSONGS"; //The SQL statement
             Statement statement = con.createStatement();
-            ResultSet rs = statement.executeQuery(sqlStatement);
-            while(rs.next())
+            ResultSet rs = statement.executeQuery(sqlStatement);//fetch the result of the SQL statement and store it.
+            while(rs.next())//While there is something listed...
             {
+                //Fetch all the relevant info
                 String name = rs.getString("name");
                 String artist = rs.getString("artist");
                 String category = rs.getString("category");
                 String time = rs.getString("time");
                 String file = rs.getString("Filelocation");
                 int id = rs.getInt("id");
+                //Create a new instance of the busioness entity Song with the values.
                 Song p = new Song(name,artist,category,time,file,id);
-                songs.add(p);
+                songs.add(p);//Add the song to the list.
             }
             
         }
@@ -112,41 +116,41 @@ public class DalController {
             Logger.getLogger(DalController.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        return songs;
+        return songs;//return the list of songs.
     }
 
     public void DeleteSong(Song song) throws SQLException {
-       String sqlStatement = "DELETE FROM PLAYLIST_SONGS WHERE SongId=? ; DELETE FROM ALLSONGS WHERE id=?; ";
+       String sqlStatement = "DELETE FROM PLAYLIST_SONGS WHERE SongId=? ; DELETE FROM ALLSONGS WHERE id=?; ";//THe SQL statment to delete a song by it's id. We delete from the PLAYLIST_SONGS table first as this table depends on the id's from the ALLSONGS table.
                
                
         try (Connection con = ds.getConnection()){
           PreparedStatement pstmt = con.prepareStatement(sqlStatement);
-        
+            //set the two ? to be the song ID in the statement.
             pstmt.setInt(1,song.getId());
             pstmt.setInt(2,song.getId());
-            pstmt.execute();
+            pstmt.execute();//execute the statement
         }
         
     }
-    
+    //Method to return all the songs connected to a certain playlist.
     public List<Song> getPlaylistSongs (String plname) {
-       List<Song> plsongs = new ArrayList();    
+       List<Song> plsongs = new ArrayList();//Create a list to store the songs in.    
          try (Connection con = ds.getConnection()){
+        //SQL statement to find the playlist ID via the name in ALLPLAYLISTS table, then use that PlaylistID to find the associated songId in the PLAYLIST_SONGS table, only to find the songs with those id's in the ALLSONGS table.
         String sqlStatement = "SELECT * FROM ALLSONGS WHERE Id IN(Select SongId FROM PLAYLIST_SONGS WHERE PlaylistId IN(SELECT PlaylistId FROM ALLPLAYLISTS WHERE Name='"+plname+"')); "; 
          Statement statement = con.createStatement();
-            ResultSet rs = statement.executeQuery(sqlStatement);
-            while(rs.next())
+            ResultSet rs = statement.executeQuery(sqlStatement);//Execute the statment.
+            while(rs.next())//WHile there info listed...
             {
-               
+               //Store all the relevant values.
                 String name = rs.getString("name");
                 String artist = rs.getString("artist");
                 String category = rs.getString("category");
                 String time = rs.getString("time");
                 String file = rs.getString("Filelocation");
                 int id = rs.getInt("id");
-                Song p = new Song(name,artist,category,time,file,id);
-                //p.setRow(rs.getRow());
-                plsongs.add(p);
+                Song p = new Song(name,artist,category,time,file,id);//create a new instance of a Song
+                plsongs.add(p); //Add the song to our list.
             }
            
         }
@@ -156,50 +160,51 @@ public class DalController {
         catch (SQLException ex) {
             Logger.getLogger(DalController.class.getName()).log(Level.SEVERE, null, ex);
         }
-         return plsongs;
+         return plsongs;//return the list.
     }
-    
+    //Method to add a song to a playilst through playlistID.
     public void addSongToPlaylist (Song song, int playlistId) throws SQLException{
-        String sqlStatement = "INSERT INTO PLAYLIST_SONGS (PlaylistId, SongId) VALUES(?, ?);";
+        String sqlStatement = "INSERT INTO PLAYLIST_SONGS (PlaylistId, SongId) VALUES(?, ?);";//SQL statment to add the PlaylistID and the SOngID to the PLAYLIST_SONGS relationship table.
                
                
         try (Connection con = ds.getConnection()){
-          PreparedStatement pstmt = con.prepareStatement(sqlStatement);
-        
+          PreparedStatement pstmt = con.prepareStatement(sqlStatement);//prepare the statement
+            //Set the values for the SQL statments.
             pstmt.setString(1,"" + playlistId + "");
-            pstmt.setInt(2,song.getId());
-            pstmt.execute();
+            pstmt.setInt(2,song.getId()); //OUr song has a method to fetch the ID.
+            pstmt.execute();//execute the statement.
         } 
     }
-    
+    //KMethod to remmove a song from one playlist.
     public void removeSongFromPlaylist (Song song, int playlistId, int id) throws SQLException{
-        String sqlStatement = "DELETE FROM PLAYLIST_SONGS WHERE PlaylistId=? AND SongId=?;";
+        String sqlStatement = "DELETE FROM PLAYLIST_SONGS WHERE PlaylistId=? AND SongId=?;";//SQL Statement to remove a song from a playlist. We need both values, as a song needs only to be removed from ONE playlsit at a time.
                
                
         try (Connection con = ds.getConnection()){
-          PreparedStatement pstmt = con.prepareStatement(sqlStatement);
-          
+          PreparedStatement pstmt = con.prepareStatement(sqlStatement);//prepare the statement.
+            //Set the vallues needed for the SQL statment.
             pstmt.setString(1,"" + playlistId + "");
             pstmt.setInt(2,id);
-            pstmt.execute();
+            pstmt.execute();//execute the statemnt.
         } 
       
     }    
-    
+    //Method to fetch all the playlists.
      public List<Playlist> getAllPlSongs() 
     { 
-        List<Playlist> playlists = new ArrayList();
+        List<Playlist> playlists = new ArrayList();//create a list to store our playlists.
         try (Connection con = ds.getConnection()){
-            String sqlStatement = "SELECT * FROM ALLPLAYLISTS";
+            String sqlStatement = "SELECT * FROM ALLPLAYLISTS";//SQL statment to fetch all the playlists created.
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sqlStatement);
-            while(rs.next())
+            while(rs.next())//WHile there is info...
             {
+                //Fetch the different values.
                 String name = rs.getString("Name");
                 int id = rs.getInt("PlaylistId");
-                Playlist p = new Playlist(name,id,0);
-                p.setRow(getNumberOfSongs(name));
-                playlists.add(p);
+                Playlist p = new Playlist(name,id,0);//Create a new instance of the business entity Playlist for now set the last row to 0,
+                p.setRow(getNumberOfSongs(name));//channge the final row to include all the number of songs in the playlist.
+                playlists.add(p);//Add to the list.
             }
             
         }
@@ -210,21 +215,22 @@ public class DalController {
             Logger.getLogger(DalController.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        return playlists;
+        return playlists;//Return the list of playlists.
     }
      
      
-     
+     //Method to count the number of songs in a playlist, by using the playlist name.
       public int getNumberOfSongs (String plname) {
     //    List<Song> plsongsc = new ArrayList();    
-         int counter = 0;
+         int counter = 0; //Initialize a counter.
          try (Connection con = ds.getConnection()){
+             //SQL statement to find all the songs associated with a certain playlist, by first finding the id of the playlist, and then finding all occurences of that playlist ID wihtin PLAYLIST_SONGS.
         String sqlStatement = "SELECT * FROM ALLSONGS WHERE Id IN(Select SongId FROM PLAYLIST_SONGS WHERE PlaylistId IN(SELECT PlaylistId FROM ALLPLAYLISTS WHERE Name='"+plname+"')); "; 
          Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sqlStatement);
-            while(rs.next())
+            while(rs.next()) //While there is info returned...
             {
-               counter++;
+               counter++;//add one to thee counter.
             }
            
         }
@@ -234,54 +240,54 @@ public class DalController {
         catch (SQLException ex) {
             Logger.getLogger(DalController.class.getName()).log(Level.SEVERE, null, ex);
         }
-         return counter;
+         return counter;//return the final count.
     }
      
      
-     
+     //Method to delete a playlist.
      public void deletePlaylist(Playlist playlist) throws SQLException
      {
-          String sqlStatement = "DELETE FROM PLAYLIST_SONGS WHERE PlaylistId=? ; DELETE FROM ALLPLAYLISTS WHERE PlaylistId=? ;";
+          String sqlStatement = "DELETE FROM PLAYLIST_SONGS WHERE PlaylistId=? ; DELETE FROM ALLPLAYLISTS WHERE PlaylistId=? ;";//SQL Statment to delete the relationship to songs first in PLAYLIST_SONGS; and then remove the playlist from ALLPLAYLISTS.
                
                
         try (Connection con = ds.getConnection()){
-          PreparedStatement pstmt = con.prepareStatement(sqlStatement);
-          
+          PreparedStatement pstmt = con.prepareStatement(sqlStatement);//prepare the statment.
+            //Add the correct id based on the business entity playlist.
             pstmt.setString(1,"" + playlist.GetId() + "");
             pstmt.setString(2,"" + playlist.GetId() + "");
-            pstmt.execute();
+            pstmt.execute();//execute the statement.
         } 
      }
      
-     
+     //MEthod to create a new playlist.
      public Playlist newPlaylist(String name) throws SQLException
      {     
        try(Connection con = ds.getConnection()){
-            String sqlIf = "INSERT INTO ALLPLAYLISTS (Name) VALUES (?);";
-            PreparedStatement pstmt = con.prepareStatement(sqlIf,Statement.RETURN_GENERATED_KEYS);
-            pstmt.setString(1, "" + name + "");
+            String sqlIf = "INSERT INTO ALLPLAYLISTS (Name) VALUES (?);";//SQL statement to insert a name into the ALLPLAYLISTS table, we don't need id as this is ann auto incremented number.
+            PreparedStatement pstmt = con.prepareStatement(sqlIf,Statement.RETURN_GENERATED_KEYS);//Prepare the statement and make sure we return the auto generated key.
+            pstmt.setString(1, "" + name + "");//insert the actual name into the statement.
           
-            pstmt.execute();
-            ResultSet rs = pstmt.getGeneratedKeys();
+            pstmt.execute();//execute the statement.
+            ResultSet rs = pstmt.getGeneratedKeys();//Store the returned result.
             rs.next();
-            int id = rs.getInt(1);
-             return new Playlist(name,id,0);
+            int id = rs.getInt(1);//Fetch the generated key as ´an int, and store it in id.
+             return new Playlist(name,id,0);//return the Playlist with the values that this business entity needs.
         
                                                }
      
      }
-     
+     //Method to edit a playlist.
  public Playlist EditPlaylist(Playlist playlist, String name, int row) throws SQLServerException, SQLException  {
         
       
         
         try(Connection con = ds.getConnection()){
-            String sqlIf = "UPDATE ALLPLAYLISTS SET name = ? WHERE PlaylistId=?";
-            PreparedStatement pstmt = con.prepareStatement(sqlIf);
-            pstmt.setString(1, "" + name + "");
-            pstmt.setInt(2,playlist.GetId());
-            pstmt.execute();
-        return new Playlist(name,playlist.GetId(),row);
+            String sqlIf = "UPDATE ALLPLAYLISTS SET name = ? WHERE PlaylistId=?"; //SQL Statement to update the playlist inside the ALLPLAYLISTS table.
+            PreparedStatement pstmt = con.prepareStatement(sqlIf);//prepare the statement.
+            pstmt.setString(1, "" + name + "");//insert the name into the statement.
+            pstmt.setInt(2,playlist.GetId());//fetch the playlist ID from out corresponding business entity, and add it to the statment.
+            pstmt.execute();//execute the statement.
+        return new Playlist(name,playlist.GetId(),row);//return a new instance of Playlist.
     }
     }
  
